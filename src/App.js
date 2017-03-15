d43496c956a6e3a6346db8c35450c98e4f747d21
kimi spencer <kimispencer@gmail.com>
2017-03-15 16:52:14 -0400

    demo w/ router

--- src/App.js
+++ src/App.js
@@ -1,21 +1,44 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+import Home from './pages/Home/Home';
+import About from './pages/About/About';
+import Topics from './pages/Topics/Topics';
 
-export default App;
+const BasicExample = () => (
+  <Router>
+    <div>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/topics">Topics</Link></li>
+      </ul>
+
+      <hr/>
+
+      <Route exact path="/" component={Home}/>
+      <Route path="/about" component={About}/>
+      <Route path="/topics" component={Topics}/>
+    </div>
+  </Router>
+)
+export default BasicExample;
+
+// import React, { Component } from 'react';
+// import './App.css';
+
+// class App extends Component {
+// 	render() {
+// 		return (
+// 			<div class="App">
+// 				<h1>hi</h1>
+// 			</div>
+// 		);
+// 	}
+// }
+
+// export default App;
