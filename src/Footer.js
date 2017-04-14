d6c11b8e70bdcfe98cc963def067167469f57cf5
kimi spencer <kimispencer@gmail.com>
2017-04-14 00:28:02 -0400

    more late night fixes/tweaks

--- src/Footer.js
+++ src/Footer.js
@@ -6,7 +6,18 @@ const Footer = (props) => {
 	return (
 		<div className="Footer">
 			<div className="text-container">
-				<h3 className="monospace underline center">hello@kimispencer.com</h3>
+				<a href="mailto:kimispencer@gmail.com"><h3 className="monospace underline center" id="Hello">hello@kimispencer.com</h3></a>
+
+				<div className="flex-col">
+					<ul id="SocialMediaList">
+						<li>
+							<a href="https://www.linkedin.com/in/kimispencer" target="_blank"><p className="grey underline">LinkedIn</p></a>
+						</li>
+						<li>
+							<a href="https://www.linkedin.com/in/kimispencer" target="_blank"><p className="grey underline">LinkedIn</p></a>
+						</li>
+					</ul>
+				</div>
 			</div>
 		</div>
 	);
